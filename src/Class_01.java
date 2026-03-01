94a85b69787b2651dbf0983aca490026c9bc0c84
tahir-madni <muhammadtahirmadni0@gmail.com>
2026-03-01 20:19:26 +0500

    Class No.2

--- src/Class_01.java
+++ src/Class_01.java
@@ -1,48 +1,46 @@
-/**
+/*
  * Date: 17 Feb 2026
  * Concepts Covered:
- * - variales types
- * - Printing
+  - Variales types
+  - Printing
  */
 
 public class Class_01 {
     /*
-     * public static void main(String[] args) {
-     * 
-     * int a = 10;
-     * int b = 2;
-     * int sum = a + b;
-     * System.out.println("Value of a is " + a);
-     * System.out.println("Sum is " + sum);
-     * 
-     * // * Variable with the term 'final' cant be updated
-     * 
-     * final int fv = 200;
-     * // fv = 20; // Not possible
-     * System.out.println("Value of a fv: " + fv);
-     * }
-     */
+    public static void main(String[] args) {
+    
+        int a = 10;
+        int b = 2;
+        int sum = a + b;
+        System.out.println("Value of a is " + a);
+        System.out.println("Sum is " + sum);
+    
+        // * Variable with the term 'final' cant be updated
+    
+        final int fv = 200;
+        // fv = 20; // Not possible
+        System.out.println("Value of a fv: " + fv);
+    }
+    */
 
     /*
-     * public static void main(String[] args) {
-     * int a = 10;
-     * float fnum = 11.5f;
-     * char letter = 'A';
-     * boolean state = true;
-     * String text = "Hello";
-     * 
-     * System.out.println(a + "\n" + fnum + "\n" + letter + "\n" + state + "\n" +
-     * text);
-     * }
-     */
+    public static void main(String[] args) {
+        int a = 10;
+        float fnum = 11.5f;
+        char letter = 'A';
+        boolean state = true;
+        String text = "Hello";
+    
+        System.out.println(a + "\n" + fnum + "\n" + letter + "\n" + state + "\n" + text);
+    }
+    */
 
-    /*
-     * public static void main(String[] args) {
-     * float a = 10.5f;
-     * int b = (int) a; // Used to change float value to int
-     * System.out.println("a is " + a);
-     * System.out.println("b is " + b);
-     * }
-     */
-
+    /* 
+    public static void main(String[] args) {
+        float a = 10.5f;
+        int b = (int) a; // Used to change float value to int
+        System.out.println("a is " + a);
+        System.out.println("b is " + b);
+    }
+    */
 }
