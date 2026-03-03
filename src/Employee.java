96e8860c984f25ae177d1c7904255914292a7e75
tahir-madni <muhammadtahirmadni0@gmail.com>
2026-03-03 15:57:27 +0500

    Class No.3

---
+++ src/Employee.java
@@ -0,0 +1,65 @@
+/*
+ * Date: 2 March 2026
+ * Class: 03
+ * Concepts Covered:
+  - Objects
+  - Scanner (User Inputs)
+ */
+
+import java.util.Scanner;
+
+public class Employee {
+
+    int id;
+    String name;
+    float salary;
+    long contact;
+    String dept;
+
+    /* 
+    public static void main(String[] args) {
+        Employee e1 = new Employee();
+    
+        e1.id = 1;
+        e1.name = "Tahir";
+        e1.salary = 50000f;
+        e1.contact = 01234567467574547474l;
+        e1.dept = "CS";
+    
+        System.out.println("Id: " + e1.id);
+        System.out.println("Name: " + e1.name);
+        System.out.println("Salary: " + e1.salary);
+        System.out.println("Contact: " + e1.contact);
+        System.out.println("Department: " + e1.dept);
+    } 
+    */
+
+    /* 
+    public static void main(String[] args) {
+        Scanner scInt = new Scanner(System.in);
+        Scanner scStr = new Scanner(System.in);
+    
+        Employee e2 = new Employee();
+    
+        System.out.print("Enter Employee Id: ");
+        e2.id = scInt.nextInt();
+        System.out.print("Enter Employee Name: ");
+        e2.name = scStr.nextLine();
+        System.out.print("Enter Employee Salary: ");
+        e2.salary = scInt.nextFloat();
+        System.out.print("Enter Employee Contact: ");
+        e2.contact = scInt.nextLong();
+        System.out.print("Enter Employee Department: ");
+        e2.dept = scStr.nextLine();
+    
+        System.out.println();
+        System.out.println("<----- Employee Details----->");
+        System.out.println("Id: " + e2.id);
+        System.out.println("Name: " + e2.name);
+        System.out.println("Salary: " + e2.salary);
+        System.out.println("Contact: " + e2.contact);
+        System.out.println("Department: " + e2.dept);
+    } 
+    */
+
+}
